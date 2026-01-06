afbddd3635f510fdd232b606308595d009f862ac
anshuvaishya18-sketch <anshuvaishya18@gmail.com>
2026-01-06 18:55:22 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,54 @@
+*{
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+}
+body {
+   width: 100vw;
+   height: 100vh;
+   background-color: black;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   gap: 30px;
+   flex-direction: column;
+}
+#logo {
+   width: 20vw;
+}
+
+#name {
+  color: rgb(202, 23, 104);
+  font-size: 45px;
+}
+#va {
+  color: rgb(202, 23, 104);
+  font-size: 45px;
+}
+#voice {
+  width: 200px;
+  display: none;
+}
+#btn {
+  width: 30%;
+  background: linear-gradient(to right,rgb(21, 145, 207),rgb(245, 41, 115));
+  padding: 9px;
+  gap: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  border-radius: 18px;
+  color: rgb(255, 255, 255);
+  box-shadow: 0 0 15px rgb(21,145,207), 0 0 15px rgb(245,41,115);
+  border: none;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+  #btn:hover {
+    box-shadow: 5px 5px 5px rgb(21, 145, 207),5px 5px 5px rgb(245, 41, 115);
+    letter-spacing: 2px;
+}
+h1 {
+  color: aliceblue;
+}
